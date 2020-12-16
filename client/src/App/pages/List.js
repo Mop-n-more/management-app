2272bbb17b897663068f12fcf5bf0f7cd9899f80
Jonathan Koh <jontkoh@gmail.com>
2020-12-15 22:26:01 -0800

    removed axios, added coments to utils/hooks.js and pages/List.js

--- client/src/App/pages/List.js
+++ client/src/App/pages/List.js
@@ -1,15 +1,14 @@
 // List.js will only be used for testing purposes
 // for connecting to api/server.js
-import React, { useState, useEffect }from 'react';
+import React from 'react';
 import { CenterContainer } from "../styles/centeredContainer";
 import { Title } from '../styles/title';
 import { useFetch } from '../utils/hooks';
 import { NextLine } from '../styles/nextLine';
-import axios from 'axios';
 
 function List() {
 
-  const [data, loading] = useFetch(
+  const [data] = useFetch(
     "http://localhost:8000/api/getList"
   );
 
