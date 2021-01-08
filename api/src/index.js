ecae0ff9161d0f66d4ec4c7c7f4f70605e400cd3
Jonathan Koh <jontkoh@gmail.com>
2021-01-07 21:07:01 -0800

    added createUserTable route to express server

--- api/src/index.js
+++ api/src/index.js
@@ -4,6 +4,7 @@ import testAPI from './routes/testAPI';
 import userRoutes from './routes/userRoutes';
 import getList from './routes/getList';
 import getUsers from '../database/queries/getUsers';
+import createUserTable from '../database/queries/createUserTable';
 
 const app = express();
 
@@ -13,6 +14,7 @@ app.use(express.urlencoded({extended: true}));
 app.use(cors());
 app.use(userRoutes);
 app.use(getUsers);
+app.use(createUserTable);
 
 // these are both used for client to server testing purposes 
 app.use(getList);
